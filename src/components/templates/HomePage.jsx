cf64c11c4f7ec0b857bceec88cdf12b4cc4e9897
sara-R98 <sarah.rahgoshay@gmail.com>
2023-12-03 19:48:54 +0330

    feat: add pagination to project

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
+import Pagination from "../modules/Pagination";
 
 const HomePage = () => {
 
     const [data , setData] = useState([]);
     const [isLoading , setIsLoading] = useState(true);
+    const [page , setPage] = useState(1);
 
     useEffect(() => {
+        setIsLoading(true);
         const fetchCoins = async () => {
-            const res= await fetch(getCoinList())
+            const res= await fetch(getCoinList(page))
             const json = await res.json()
             setData(json);
             setIsLoading(false)
         }
         fetchCoins();
-    }, [])
+    }, [page])
 
     return (
         <div>
            <TableCoin coins= {data} isLoading={isLoading} />
+           <Pagination page={page} setPage={setPage} />
         </div>
     );
 };
